3c70a79618801be0a47f5d76b60fa0533b51020b
Samuel Izquierdo <107511912+Samu-izq@users.noreply.github.com>
2025-03-09 11:26:02 -0500

    add testing

---
+++ test/bg.hackathon.alphahacker.test/BusquedaControllerTest.cs
@@ -0,0 +1,63 @@
+﻿using Moq;
+using Microsoft.AspNetCore.Mvc;
+using bg.hackathon.alphahackers.api.controller;
+using bg.hackathon.alphahackers.application.data.interfaces.services;
+using bg.hackathon.alphahackers.application.models.dtos;
+using bg.hackathon.alphahackers.domain.models;
+
+namespace bg.hackathon.alphahacker.test
+{
+    public class BusquedaControllerTests
+    {
+        private readonly Mock<IBusquedaServices> _busquedaServicesMock;
+        private readonly BusquedaController _controller;
+
+        public BusquedaControllerTests()
+        {
+            _busquedaServicesMock = new Mock<IBusquedaServices>();
+            _controller = new BusquedaController(_busquedaServicesMock.Object);
+        }
+
+        [Fact]
+        public async Task ObtenerPerfil_DeberiaRetornarOkConDatos()
+        {
+            // Arrange
+            string query = "Medicina";
+            string pais = "Ecuador";
+            string ciudad = "Quito";
+            string provincia = "Pichincha";
+
+            var empresas = new List<ClienteDTo>
+        {
+            new ClienteDTo { Id_Cliente = 1, Nombre = "Clinica Quito", Categoria = "Medicina" },
+            new ClienteDTo { Id_Cliente = 2, Nombre = "Farmacia Salud", Categoria = "Medicina" }
+        };
+
+            _busquedaServicesMock.Setup(s => s.ObtenerBusqueda(query, ciudad, pais, provincia))
+                                 .ReturnsAsync(empresas);
+
+            // Act
+            var result = await _controller.ObtenerPerfil(query, pais, ciudad, provincia);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var response = Assert.IsType<MsDtoResponse<List<ClienteDTo>>>(okResult.Value);
+            Assert.Equal(empresas, response.data);
+        }
+
+        [Fact]
+        public async Task ObtenerPerfil_DeberiaRetornarBadRequestSiFalla()
+        {
+            // Arrange
+            string query = "Tecnología";
+            _busquedaServicesMock.Setup(s => s.ObtenerBusqueda(query, null, null, null))
+                                 .ThrowsAsync(new Exception("Error en el servicio"));
+
+            // Act
+            var result = await _controller.ObtenerPerfil(query, null, null, null);
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+    }
+}
---
+++ test/bg.hackathon.alphahacker.test/PerfilControlerTest.cs
@@ -0,0 +1,89 @@
+﻿using Moq;
+using Microsoft.AspNetCore.Mvc;
+using bg.hackathon.alphahackers.api.controller;
+using bg.hackathon.alphahackers.application.data.interfaces.services;
+using bg.hackathon.alphahackers.domain.entities.pyme;
+using bg.hackathon.alphahackers.domain.models;
+using bg.hackathon.alphahackers.domain.entities.enums;
+
+namespace bg.hackathon.alphahacker.test
+{
+    public class PerfilControllerTests
+    {
+        private readonly Mock<IPerfilServices> _perfilServicesMock;
+        private readonly PerfilController _controller;
+
+        public PerfilControllerTests()
+        {
+            _perfilServicesMock = new Mock<IPerfilServices>();
+            _controller = new PerfilController(_perfilServicesMock.Object);
+        }
+
+        [Fact]
+        public async Task ObtenerLineaCredito_DeberiaRetornarOkConDatos()
+        {
+            // Arrange
+            int codigoCliente = 1123;
+            var lineaCredito = new LineaCredito { Total_Credito = 10000, Utilizado = 5000, Estado = EstadoLineaCredito.ACTIVO };
+            _perfilServicesMock.Setup(s => s.ObtenerLineaCredito(codigoCliente))
+                               .ReturnsAsync(lineaCredito);
+
+            // Act
+            var result = await _controller.ObtenerLineaCredito(codigoCliente);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var response = Assert.IsType<MsDtoResponse<LineaCredito>>(okResult.Value);
+            Assert.Equal(lineaCredito, response.data);
+        }
+
+        [Fact]
+        public async Task ObtenerLineaCredito_DeberiaRetornarBadRequestSiFalla()
+        {
+            // Arrange
+            int codigoCliente = 1123;
+            _perfilServicesMock.Setup(s => s.ObtenerLineaCredito(codigoCliente))
+                               .ThrowsAsync(new Exception("Error en el servicio"));
+
+            // Act
+            var result = await _controller.ObtenerLineaCredito(codigoCliente);
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task ObtenerPerfil_DeberiaRetornarOkConDatos()
+        {
+            // Arrange
+            int codigoCliente = 1123;
+            var cliente = new Cliente();
+            _perfilServicesMock.Setup(s => s.ObtenerPerfil(codigoCliente))
+                               .ReturnsAsync(cliente);
+
+            // Act
+            var result = await _controller.ObtenerPerfil(codigoCliente);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var response = Assert.IsType<MsDtoResponse<Cliente>>(okResult.Value);
+            Assert.Equal(cliente, response.data);
+        }
+
+        [Fact]
+        public async Task ObtenerPerfil_DeberiaRetornarBadRequestSiFalla()
+        {
+            // Arrange
+            int codigoCliente = 1123;
+            _perfilServicesMock.Setup(s => s.ObtenerPerfil(codigoCliente))
+                               .ThrowsAsync(new Exception("Error en el servicio"));
+
+            // Act
+            var result = await _controller.ObtenerPerfil(codigoCliente);
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+    }
+
+}
