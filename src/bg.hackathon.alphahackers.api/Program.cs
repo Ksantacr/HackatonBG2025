d33e6e0b144366abae46007fcc650992ba4d5a3c
Samuel Izquierdo <107511912+Samu-izq@users.noreply.github.com>
2025-03-09 11:08:49 -0500

    add cors

--- src/bg.hackathon.alphahackers.api/Program.cs
+++ src/bg.hackathon.alphahackers.api/Program.cs
@@ -8,6 +8,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Configurar servicios
 builder.Services.AddControllers();  // Habilitar controladores MVC
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", builder =>
+    {
+        builder.AllowAnyOrigin()   // Permitir cualquier origen
+               .AllowAnyMethod()   // Permitir cualquier verbo HTTP
+               .AllowAnyHeader();  // Permitir cualquier encabezado
+    });
+});
+
+
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
